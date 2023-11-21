3cd1a788d3fccd5326ad106ee2d60f65c52b6eb1
Vladimir-Mokanj <vlada.mokanj@gmail.com>
2023-11-21 18:10:08 +0100

    Decoupled the main ui controller

--- Assets/_Prototype/UI/MainMenuUiController.cs
+++ Assets/_Prototype/UI/MainMenuUiController.cs
@@ -1,73 +1,38 @@
-using System;
-using FTS.UI.Screens;
-using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace FTS.UI
 {
-    public class MainMenuUiController : MonoBehaviour, IButtonHandler<IMenuButtonUi>
+    public class MainMenuUiController : UiMainController
     {
-        [SerializeField] private float _menuDisplaySpeed = 0.35f;
-        [SerializeField] private Color _defaultColor;
-        [SerializeField] private Color _hoveredColor;
-        [SerializeField] private Color _selectedColor;
-        
         private IMenuButtonUi _currentButton;
-        private IScreen _currentScreen;
         private EventSystem _eventSystem;
         
-        private Action _closeRequestAction;
-        private Action<IScreen> _openRequestAction;
-
-        private void Awake() => 
+        private void Awake() =>
             _eventSystem = EventSystem.current;
-
-        public void OnEnter(IMenuButtonUi button)
+        
+        public override void OnEnter(IMenuButtonUi button)
         {
             _eventSystem.SetSelectedGameObject((button as MenuButtonUi)?.gameObject);
             button.SetTextColor(_currentButton == button ? _selectedColor : _hoveredColor);
         }
 
-        public void OnExit(IMenuButtonUi button)
+        public override void OnExit(IMenuButtonUi button)
         {
             button.SetTextColor(_currentButton == button ? _selectedColor : _defaultColor);
         }
 
-        public void OnClick(IMenuButtonUi button)
+        public override void OnClick(IMenuButtonUi button)
         {
             _currentButton?.SetTextColor(button == _currentButton ? _hoveredColor : _defaultColor);
             _currentButton = button == _currentButton ? null : button;
-            
+
             if (_currentScreen != null)
                 HideScreen(_currentScreen);
-            
+
             if (_currentButton == null)
                 return;
-            
+
             ShowScreen(_currentButton.OnInteract(_selectedColor));
-        }
-        
-        private void ShowScreen(IScreen screen)
-        {
-            _closeRequestAction = () => HideScreen(screen);
-            _openRequestAction = s =>
-            {
-                HideScreen(screen);
-                ShowScreen(s);
-            };
-            screen.OnRequestToClose += _closeRequestAction;
-            screen.OnRequestToOpen += _openRequestAction;
-            screen.Show(_menuDisplaySpeed);
-            _currentScreen = screen;
-        }
-        
-        private void HideScreen(IScreen screen)
-        {
-            screen.OnRequestToClose -= _closeRequestAction;
-            screen.OnRequestToOpen -= _openRequestAction;
-            _closeRequestAction = null;
-            screen.Hide(_menuDisplaySpeed);
-            _currentScreen = null;
         }
     }
 }
